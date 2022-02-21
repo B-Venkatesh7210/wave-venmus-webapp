3f818a4206839e79fbca0a2b8a950b6c354b4e1e
B-Venkatesh7210 <67518179+B-Venkatesh7210@users.noreply.github.com>
2022-02-21 16:51:56 +0530

    Metamask authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import * as React from "react";
+import React, {useEffect} from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+
+  const checkIfWalletIsConnected = () => {
+    
+    //First make sure we have access to window.ethereum
+    const {ethereum} = window;
+
+    if(!ethereum){
+      console.log("Make sure you have Metamask")
+    }
+    else{
+      console.log("We have the ethereum object")
+    }
+
+  }
+
+  useEffect(()=> {
+    checkIfWalletIsConnected()
+  }, [])
+
+  
 
   const wave = () => {
     
